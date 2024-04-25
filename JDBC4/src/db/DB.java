416f91588a1d632156e44e9cd37e860b40ec0a8c
Edwin Yassunaga <edwinjunyassunaga@gmail.com>
2024-04-25 13:00:57 -0300

    Database with JDBC - delete data

--- JDBC4/src/db/DB.java
+++ JDBC4/src/db/DB.java
@@ -19,8 +19,7 @@ public class DB {
                 Properties props = loadProperties();
                 String url = props.getProperty("dburl");
                 conn = DriverManager.getConnection(url, props);
-            }
-            catch (SQLException e) {
+            } catch (SQLException e) {
                 throw new DbException(e.getMessage());
             }
         }
@@ -42,8 +41,7 @@ public class DB {
             Properties props = new Properties();
             props.load(fs);
             return props;
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new DbException(e.getMessage());
         }
     }
